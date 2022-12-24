3407646f681de74669b5cfac53b33790bc9b5c78
Tracich <param.singh.15.16@gmail.com>
2022-12-23 17:01:04 -0800

    Includes summarised collation of functions in cachematrix.R file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Provides an intermediate list that can be assigned to a variable
+## to which a square matrix, which has a determinant of zero, and it's inverse can be assigned
+## and from which the assigned square  matrix and its inverse can be retrieved.
+## Inverse of included matrix can also be obtained.
 
-## Write a short comment describing this function
+
+## Given an argument the function returns a list.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv <- matrix()
+  
+  set <- function(y) {
+    x <<- y
+    inv <- matrix()
+  }
+  get <- function() {x}
+  
+  setinv <- function(xinverse) {inv <<- xinverse}
+  getinv <- function() {inv}
+  
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+##Returns an inverse of a square matrix with determinant value of zero.
+##The matrix is assigned to variable x, which is a list returned by makeCacheMatrix function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheinverse <- function (x) {
+  
+  inv <- x$getinv()
+  if(!(dim(inv) == c(1,1))[1]){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
+
 }
